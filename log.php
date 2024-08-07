ba700543406740c0b3d1cdbcc2a0c132dc887a0e
Hellochampa <imperus.chachocky334@gmail.com>
2024-08-07 16:38:53 +0200

    Update log.php

--- log.php
+++ log.php
@@ -1,13 +1,11 @@
 <?php
 // Handle CORS preflight requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: conflictnations.site");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
     header("Access-Control-Allow-Credentials: true");
-    // Respond with 200 OK to indicate preflight check is successful
-    http_response_code(200);
-    exit();
+    exit(0);
 }
 
 // Allow from any origin for actual requests
@@ -17,7 +15,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // Log "hello" to the log file
-$logfile = 'logs.txt';
+$logfile = 'logs/logs.txt';
 $message = "hello\n";
 file_put_contents($logfile, $message, FILE_APPEND);
 
