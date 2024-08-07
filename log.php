08c1bf5fc7ede53383eb2de7bff514a58644dedc
Hellochampa <imperus.chachocky334@gmail.com>
2024-08-07 16:26:37 +0200

    Update log.php

--- log.php
+++ log.php
@@ -5,7 +5,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
     header("Access-Control-Allow-Credentials: true");
-    exit(0);
+    // Respond with 200 OK to indicate preflight check is successful
+    http_response_code(200);
+    exit();
 }
 
 // Allow from any origin for actual requests
